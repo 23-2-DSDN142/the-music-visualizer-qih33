7c6d338da0874d29bfba2437f23924c214201df6
Qing Qing He <qingqinghe916@hotmail.com>
2023-09-28 10:50:01 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,4 +1,7 @@
-
+var leftcircle_posx = 540/2;
+var rightcircle_posx = 540/2;
+var isLeftcircle = false;
+var isRighrcircle = false;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
@@ -16,40 +19,45 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   fill(colorLerp);
   ellipse(width/2, height/2, rectSize, rectSize);
-
-
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
  
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
+  right_circle(rectSize);
+  left_circle(rectSize);
+  
    // display "words"
    textAlign(CENTER);
    textSize(vocal);
    text(words, width/2, height/3);
+}
+
+
+function right_circle(rectSize){
+  //right circle
+    if (rightcircle_posx < 450){
+        rightcircle_posx++
+        ellipse(rightcircle_posx, height/2, rectSize, rectSize);
+    }
+
+    if (rightcircle_posx === 450){
+        isRighrcircle = true;
+    }
+    if (isRighrcircle)
+    {
+      ellipse(rightcircle_posx, height/2, rectSize, rectSize);
+    }
+
+}
+
+function left_circle(rectSize){
+  if (leftcircle_posx > 90){
+    leftcircle_posx--
+    ellipse(leftcircle_posx, height/2, rectSize, rectSize);
+  }
+
+  if (leftcircle_posx === 90){
+    isLeftcircle = true;
+  }
+  if (isLeftcircle)
+  {
+    ellipse(leftcircle_posx, height/2, rectSize, rectSize);
+  }
 }
